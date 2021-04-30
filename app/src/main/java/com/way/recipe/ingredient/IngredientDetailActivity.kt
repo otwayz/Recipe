2a144e523d81e6f9461af583210399f846d3b498
guangcheng.zhang <guangcheng.zhang@shanbay.com>
2021-04-30 18:00:05 +0800

    Detail page

---
+++ app/src/main/java/com/way/recipe/ingredient/IngredientDetailActivity.kt
@@ -0,0 +1,16 @@
+package com.way.recipe.ingredient
+
+import android.os.Bundle
+import com.way.recipe.R
+import com.way.recipe.base.BaseActivity
+
+/**
+ * Created by Otway on 2021/4/27.
+ */
+class IngredientDetailActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ingredient_detail)
+    }
+}
